1b4765cc2701ef93602614a9fe69423a8a42b0e1
Clayton Dunwell <clayton.dunwell@gmail.com>
2026-05-15 17:58:01 -0400

    deprecation fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'node:path'
 
 const isGithubPages =
     process.env.NODE_ENV === 'production'
